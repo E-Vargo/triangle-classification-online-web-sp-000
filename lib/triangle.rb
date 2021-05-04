863376b87b660f442cda52fa85658c4cb14f5510
Elliott Vargo <vargoelliott@gmail.com>
2021-05-04 23:51:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,19 @@
 class Triangle
   # write code here
-  attr_accessor :leg1, :leg2, :hypotensuse, :type
+  attr_accessor :leg1, :leg2, :leg3, :type
 
-  def initialize(leg1, leg2, hypotenuse)
+  def initialize(leg1, leg2, leg3)
     @leg1 = leg1
     @leg2 = leg2
-    @hypotenuse = hypotenuse
+    @leg3 = leg3
   end
 
   def kind
-    if @leg1 == 0 || @leg2 == 0 || @hypotenuse == 0
+    if @leg1 == 0 || @leg2 == 0 || @leg3 == 0
       raise TriangleError
-    elsif @leg1 == @leg2 && @leg2 == @hypotenuse
+    elsif @leg1 == @leg2 && @leg2 == @leg3
       :equilateral
-    elsif @leg1 == @leg2 && leg2 != @hypotenuse
+    elsif @leg1 == @leg2 && leg2 != @leg3
       :isosceles
     end
   end
