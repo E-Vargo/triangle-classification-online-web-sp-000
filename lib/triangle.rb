a0469602d72d1b8649894cbc9b6bf073aeb88058
Elliott Vargo <vargoelliott@gmail.com>
2021-05-04 23:38:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,18 @@
 class Triangle
   # write code here
-  attr_accessor :leg1, :leg2, :hypotensuse
+  attr_accessor :leg1, :leg2, :hypotensuse, :type
 
   def initialize(leg1, leg2, hypotenuse)
     @leg1 = leg1
     @leg2 = leg2
     @hypotenuse = hypotenuse
+  end
+
+  def kind
+    if @leg1 == 0 || @leg2 == 0 || @hypotenuse == 0
+      raise TriangleError
+    elsif 
+    end
   end
 
  class TriangleError < StandardError
