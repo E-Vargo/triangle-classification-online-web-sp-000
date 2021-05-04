4d9976b623b53d4e8bef6677b892a5e83c044080
Elliott Vargo <vargoelliott@gmail.com>
2021-05-04 23:30:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,11 @@
 class Triangle
   # write code here
+  attr_accessor :leg1, :leg2, :hypotensuse
+
+  def initialize(leg1 = 0, leg2 = 0, hypotenuse = 0)
+
+  end
  class TriangleError < StandardError
 
  end
 end
- 
